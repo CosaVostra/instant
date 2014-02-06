3396c66ab06b958919f40f166ce96f2cffbc98ed
Johan Colliez <jcolliez@linterweb.fr>
2014-02-06 14:35:51 +0100

    modif User + conf security

--- src/Cosa/Instant/UserBundle/Controller/DefaultController.php
+++ src/Cosa/Instant/UserBundle/Controller/DefaultController.php
@@ -9,25 +9,26 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 class DefaultController extends Controller
 {
     public function loginAction()
-    {
+    {//var_dump($this->get('session'));
+var_dump($this->get('security.context')->getToken());//->getUser());
         return $this->render('CosaInstantUserBundle:Default:login.html.twig');
     }
 
     public function logoutAction(Request $request)
-    {
+    {var_dump($this->get('session'));exit;
         $request->getSession()->clear();
         return $this->redirect($this->generateUrl('homepage'));
     }
 
     public function loginCheckAction(Request $request)
     {
-        //$request->get('
         return $this->redirect($this->generateUrl('homepage'));
-//        return $this->render('CosaInstantUserBundle:Default:login.html.twig');
     }
 
     public function connectTwitterAction()
-    {   
+    {
+        if($this->get('security.context')->isGranted('ROLE_USER'))
+            return $this->redirect($this->generateUrl('homepage'));
         $request = $this->get('request');
         $twitter = $this->get('fos_twitter.service');
         $authURL = $twitter->getLoginUrl($request);
--- src/Cosa/Instant/UserBundle/Entity/User.php
+++ src/Cosa/Instant/UserBundle/Entity/User.php
@@ -38,6 +38,46 @@ class User extends BaseUser
      */
     protected $twitter_access_token_secret;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $profile_image_url;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $twitter_realname;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $twitter_location;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $optin;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $created_at;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $updated_at;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $login_count;
+
+    /**
+     * @ORM\Column(type="string", length=4)
+     */
+    protected $lang;
+
     public function __construct()
     {
         parent::__construct();
@@ -125,4 +165,188 @@ class User extends BaseUser
         {
             return $this->twitter_access_token_secret;
         }
+
+    /**
+     * Set profile_image_url
+     *
+     * @param string $profileImageUrl
+     * @return User
+     */
+    public function setProfileImageUrl($profileImageUrl)
+    {
+        $this->profile_image_url = $profileImageUrl;
+
+        return $this;
+    }
+
+    /**
+     * Get profile_image_url
+     *
+     * @return string 
+     */
+    public function getProfileImageUrl()
+    {
+        return $this->profile_image_url;
+    }
+
+    /**
+     * Set twitter_realname
+     *
+     * @param string $twitterRealname
+     * @return User
+     */
+    public function setTwitterRealname($twitterRealname)
+    {
+        $this->twitter_realname = $twitterRealname;
+
+        return $this;
+    }
+
+    /**
+     * Get twitter_realname
+     *
+     * @return string 
+     */
+    public function getTwitterRealname()
+    {
+        return $this->twitter_realname;
+    }
+
+    /**
+     * Set twitter_location
+     *
+     * @param string $twitterLocation
+     * @return User
+     */
+    public function setTwitterLocation($twitterLocation)
+    {
+        $this->twitter_location = $twitterLocation;
+
+        return $this;
+    }
+
+    /**
+     * Get twitter_location
+     *
+     * @return string 
+     */
+    public function getTwitterLocation()
+    {
+        return $this->twitter_location;
+    }
+
+    /**
+     * Set optin
+     *
+     * @param string $optin
+     * @return User
+     */
+    public function setOptin($optin)
+    {
+        $this->optin = $optin;
+
+        return $this;
+    }
+
+    /**
+     * Get optin
+     *
+     * @return string 
+     */
+    public function getOptin()
+    {
+        return $this->optin;
+    }
+
+    /**
+     * Set created_at
+     *
+     * @param \DateTime $createdAt
+     * @return User
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->created_at = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get created_at
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Set updated_at
+     *
+     * @param \DateTime $updatedAt
+     * @return User
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updated_at = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updated_at
+     *
+     * @return \DateTime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * Set login_count
+     *
+     * @param integer $loginCount
+     * @return User
+     */
+    public function setLoginCount($loginCount)
+    {
+        $this->login_count = $loginCount;
+
+        return $this;
+    }
+
+    /**
+     * Get login_count
+     *
+     * @return integer 
+     */
+    public function getLoginCount()
+    {
+        return $this->login_count;
+    }
+
+    /**
+     * Set lang
+     *
+     * @param string $lang
+     * @return User
+     */
+    public function setLang($lang)
+    {
+        $this->lang = $lang;
+
+        return $this;
+    }
+
+    /**
+     * Get lang
+     *
+     * @return string 
+     */
+    public function getLang()
+    {
+        return $this->lang;
+    }
 }
