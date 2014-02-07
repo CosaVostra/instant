92c03ed8fd1e8c43f2b77f371efa5c85244d5bc2
Johan Colliez <jcolliez@linterweb.fr>
2014-02-07 11:42:19 +0100

    ajout entity keyword, timeline, twittos

---
+++ src/Cosa/Instant/TimelineBundle/Entity/Keyword.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Cosa\Instant\TimelineBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class Keyword
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $keyword;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Cosa\Instant\TimelineBundle\Entity\Instant", cascade={"remove","persist"}, inversedBy="keywords")
+     */
+    private $instant;
+
+    /**
+     * Set keyword
+     *
+     * @param string $keyword
+     * @return Keyword
+     */
+    public function setKeyword($keyword)
+    {
+        $this->keyword = $keyword;
+
+        return $this;
+    }
+
+    /**
+     * Get keyword
+     *
+     * @return string 
+     */
+    public function getKeyword()
+    {
+        return $this->keyword;
+    }
+
+    /**
+     * Set instant
+     *
+     * @param \Cosa\Instant\TimelineBundle\Entity\Instant $instant
+     * @return Keyword
+     */
+    public function setInstant(\Cosa\Instant\TimelineBundle\Entity\Instant $instant = null)
+    {
+        $this->instant = $instant;
+
+        return $this;
+    }
+
+    /**
+     * Get instant
+     *
+     * @return \Cosa\Instant\TimelineBundle\Entity\Instant 
+     */
+    public function getInstant()
+    {
+        return $this->instant;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
---
+++ src/Cosa/Instant/TimelineBundle/Entity/Twittos.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Cosa\Instant\TimelineBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class Twittos
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=1000, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Cosa\Instant\UserBundle\Entity\User", cascade={"remove","persist"}, inversedBy="Twittos")
+     */
+    private $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Cosa\Instant\TimelineBundle\Entity\Instant", cascade={"remove","persist"}, inversedBy="twittos")
+     */
+    private $instant;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Twittos
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \Cosa\Instant\UserBundle\Entity\User $user
+     * @return Twittos
+     */
+    public function setUser(\Cosa\Instant\UserBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \Cosa\Instant\UserBundle\Entity\User 
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set instant
+     *
+     * @param \Cosa\Instant\TimelineBundle\Entity\Instant $instant
+     * @return Twittos
+     */
+    public function setInstant(\Cosa\Instant\TimelineBundle\Entity\Instant $instant = null)
+    {
+        $this->instant = $instant;
+
+        return $this;
+    }
+
+    /**
+     * Get instant
+     *
+     * @return \Cosa\Instant\TimelineBundle\Entity\Instant 
+     */
+    public function getInstant()
+    {
+        return $this->instant;
+    }
+}
