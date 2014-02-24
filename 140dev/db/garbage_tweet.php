423304d3bcf3743a4e39d6a855fb73af87f3837a
Johan Colliez <jcolliez@linterweb.fr>
2014-02-24 10:43:50 +0100

    fix robot garbage

--- 140dev/db/garbage_tweet.php
+++ 140dev/db/garbage_tweet.php
@@ -2,9 +2,9 @@
 require_once('db_config.php');
 $dbh = mysqli_connect($db_host, $db_user, $db_password, $db_name);
 $query=" DELETE t.* FROM Tweet t LEFT JOIN instant_tweet it ON it.tweet_id = t.id WHERE it.tweet_id IS NULL;";
-mysqli_query($this->getDbh(),$query);
-if ($this->error_msg = mysqli_error($this->getDbh())) {
-  echo $this->error_msg;
+mysqli_query($dbh,$query);
+if ($error_msg = mysqli_error($dbh)) {
+  echo $error_msg;
 }else{
   echo 'OK';
 }
