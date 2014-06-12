e45e0b8a525d2cedf6aea8972189256974af4fec
Pierre STEFANI <pierre@cosavostra.com>
2014-06-12 12:21:44 +0200

    get tweets

--- 140dev/db/loop_get_tweets.sh
+++ 140dev/db/loop_get_tweets.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/create/instant/140dev/db
 while true; do
-  php /home/create/instant/140dev/db/get_tweets.php > /home/create/instant/140dev/db/get_tweets.log 2>&1
+  php /home/create/instant/140dev/db/get_tweets.php > /dev/null 2>&1
 done
